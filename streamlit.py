d33e879d95efddaefeeb0db2a9bc8894a3f5acb9
rcondes <122294576+rcondes@users.noreply.github.com>
2023-01-10 09:42:33 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,15 @@
 import streamlit
 
-streamlit.title('Haciendo cositas')
+streamlit.title('Título: Haciendo cositas')
 
-streamlit.header('My new code has no sense')
+streamlit.header('Header 1')
 
+streamlit.text('My new code has no sense')
 streamlit.text('----------------')
 streamlit.text('--Example code--')
 streamlit.text('----------------')
+
+streamlit.header('Header 2')
+
 streamlit.text('Y ahora unas moñerías varias')
 streamlit.text('🥣 🥗 🐔 🥑🍞')
