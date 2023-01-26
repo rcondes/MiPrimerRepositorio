f402a5ee151af93beab568f121651678434428b1
rcondes <122294576+rcondes@users.noreply.github.com>
2023-01-26 11:52:38 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -29,9 +29,10 @@ streamlit.multiselect ("Seleccione sus frutas:", list(my_fruit_list.index),['Avo
 
 streamlit.dataframe(my_fruit_list)
 
+#New Section to display frutyvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
